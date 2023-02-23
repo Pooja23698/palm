352901614e01f84a1e4408eac920e16e6b302575
Pooja23698 <102891653+Pooja23698@users.noreply.github.com>
2023-02-23 20:06:07 +0530

    Add files via upload

---
+++ add.sh
@@ -0,0 +1,6 @@
+read -p "Enter 1st number;" x
+read -p "Enter 2nd number;" y
+sum=$(( $x + $y ))
+echo "sum is: $sum"
+fi
+
---
+++ if.sh
@@ -0,0 +1,7 @@
+read -p "Enter the number;" a
+read -p "Enter the number;" b
+if [ $a -gt $b ]
+then
+echo $a is greater than $b
+fi
+
